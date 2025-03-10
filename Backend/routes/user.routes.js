a5758bcf480e4a1fdc5dc81f50626007f6ddd856
HeatblasterAA <amanarora_60@yahoo.com>
2025-03-11 01:27:44 +0530

    Created controller and routes for user creation

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+//First we validate the data hence we use express validator
+const { body } = require("express-validator");
+
+const userController = require("../controllers/user.controller");
+
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name at least characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must atleast be 6 characters long"),
+  ]
+
+  //user Creation!
+);
+
+module.exports = router;
