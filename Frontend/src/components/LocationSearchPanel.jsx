dacb4d9523a5de0d1dea92a1ea66adfe799dd78f
HeatblasterAA <amanarora_60@yahoo.com>
2025-03-26 21:43:59 +0530

    Created basic home page UI for user

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = (props) => {
+  // Sample array for location
+
+  console.log(props);
+
+  const locations = [
+    'Tandon 177 mnnit prayagraj',
+    'NBH 123 prayagraj',
+    'Banaras GHAT 123',
+    'Sangam ghat'
+  ];
+
   return (
-    <div>LocationSearchPanel</div>
-  )
-}
+    <div onClick={() => {
+      props.setVehiclePanel(true);
+      props.setPanelOpen(false);
+    }}>
+      {locations.map((elem, index) => (
+        <div
+          key={index} // Adding unique key
+          className='flex gap-4 my-2 items-center border-2 p-3 border-gray-50 active:border-black rounded-xl justify-start'
+        >
+          <h2 className='bg-[#eee] flex h-10 items-center justify-center w-10 rounded-full'>
+            <i className="ri-map-pin-2-fill"></i>
+          </h2>
+          <h4 className='font-medium'>{elem}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default LocationSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -12,6 +12,8 @@ const Home = () => {
 
   const panelRef = useRef(null)
   const panelCLoseRef = useRef(null);
+  const [vechiclePanel, setVehiclePanel] = useState(false)
+  const vechiclePanelRef = useRef(null);
 
 
   const submitHandler = (e) => {
@@ -40,11 +42,26 @@ const Home = () => {
     }
   }, [panelOpen]);
 
+  useGSAP(function () {
+
+    if (vechiclePanel) {
+      gsap.to(vechiclePanelRef.current, {
+        transform: 'translateY(0)'
+
+      })
+    } else {
+      gsap.to(vechiclePanelRef.current, {
+        transform: 'translateY(100%)'
+
+      })
+    }
+  }, [vechiclePanel])
+
   return (
     <div className='h-full relative overflow-hidden'>
-      <div className="absolute top-0 left-0  text-black font-bold text-2xl p-5 z-10">
+      <div className=" w-16 absolute top-0 left-0  text-black font-bold text-2xl p-5 z-10">
         Raahi...
-      </div>
+      </div >
       {/* <b className='w-16 absolute left-5 top-5' >Raahi..</b> */}
 
 
@@ -61,7 +78,7 @@ const Home = () => {
 
         <div className='h-[30%] bg-white p-6 '>
           <h5 className='absolute opacity-0 right-6 top-6 text-2xl' ref={panelCLoseRef} onClick={() => setPanelOpen(false)}>
-            <i class="ri-arrow-down-wide-fill"></i>
+            <i className="ri-arrow-down-wide-fill"></i>
           </h5>
           <h4 className='text-3xl font-semibold'>Find Your Trip</h4>
           <form onSubmit={(e) => submitHandler(e)}>
@@ -85,8 +102,66 @@ const Home = () => {
         </div>
 
         <div ref={panelRef} className='h-0  bg-white' >
-          <LocationSearchPanel />
+          <LocationSearchPanel setPanelOpen={setPanelOpen} setVehiclePanel={setVehiclePanel} />
         </div>
+      </div>
+
+      <div ref={vechiclePanelRef} className='fixed w-full z-10 bg-white bottom-0 translate-y-full px-3 py-10 pt-14'>
+        <h5 className='p-1 text-center w-[93%] absolute top-0'> <i className="text-3xl text-gray-300 ri-arrow-down-wide-fill" onClick={() => { setVehiclePanel(false) }}></i></h5>
+
+        <h3 className='text-2xl font-semibold mb-5'>Choose your ride</h3>
+
+        <div className=' flex mb-2 p-3 z-10 items-center border-2 bg-gray-100 active:border-black border-rounded-xl  justify-between'>
+          <img className='h-12' src="https://cdn.creazilla.com/cliparts/78555/taxi-clipart-lg.png" alt=""></img>
+          <div className=' w-1/2' >
+            <h4 className='font-medium text-base'>RaahiGo <span><i className="ri-user-line"></i>
+              4
+            </span>
+            </h4>
+            <h5 className='font-medium text-sm'>2 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable compact rides</p>
+
+          </div>
+          <h2 className='text-lg font-semibold'>₹195.00</h2>
+
+
+        </div>
+
+        <div className=' flex mb-2 p-3 z-10 items-center border-2 bg-gray-100 active:border-black border-rounded-xl  justify-between'>
+          <img className='h-12' src="https://thumbs.dreamstime.com/b/linear-simple-motorcycle-separated-white-space-ready-to-work-easy-display-any-devices-website-113983541.jpg?w=768" alt=""></img>
+          <div className=' w-1/2' >
+            <h4 className='font-medium text-base'>RaahiMoto <span><i className="ri-user-line"></i>
+              1
+            </span>
+            </h4>
+            <h5 className='font-medium text-sm'>1 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable motor rides</p>
+
+          </div>
+          <h2 className='text-lg font-semibold'>₹65.00</h2>
+
+
+        </div>
+
+        <div className=' flex mb-2 p-3 z-10 items-center border-2 bg-gray-100 active:border-black border-rounded-xl  justify-between'>
+          <img className='h-12' src="https://img.freepik.com/premium-vector/auto-rickshaw-clipart-vector-art-illustration_761413-24121.jpg?w=826" alt=""></img>
+          <div className=' w-1/2' >
+            <h4 className='font-medium text-base'>RaahiAuto <span><i className="ri-user-line"></i>
+              3
+            </span>
+            </h4>
+            <h5 className='font-medium text-sm'>4 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable Auto rides</p>
+
+          </div>
+          <h2 className='text-lg font-semibold'>₹125.00</h2>
+
+
+        </div>
+
+
+
+
       </div>
 
 
